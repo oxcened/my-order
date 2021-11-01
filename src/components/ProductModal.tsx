739da2b72a2bb4c24ca551667bc02710d73d8926
Alen Ajam <alen.ajam@live.com>
2021-11-01 11:59:19 +0100

    modal animation, enabled tailwind jit

--- src/components/ProductModal.tsx
+++ src/components/ProductModal.tsx
@@ -3,19 +3,37 @@ import { MinusIcon, PlusIcon } from '@heroicons/react/solid';
 import Button from './Button';
 import Modal from './Modal';
 import * as React from 'react';
-import { ComponentPropsWithoutRef, useEffect, useState } from 'react';
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from 'react';
 import { Product } from '../models/Product';
+
+const ANIM_DURATON_MS = 350;
 
 const ProductModal = ({ product, onSubmit, ...props }: {
   product?: Product,
   onSubmit?: (quantity: number) => void;
 } & ComponentPropsWithoutRef<typeof Modal>) => {
   const [quantity, setQuantity] = useState(1);
+  const [cachedProduct, setCachedProduct] = useState<Product>();
+  const cacheTimeout = useRef<number>();
 
   useEffect(() => {
     if (!product) {
       // Clean quantity on product change
       setQuantity(1);
+    }
+
+    if (cacheTimeout.current) {
+      // Clear any previous timeout
+      window.clearTimeout(cacheTimeout.current);
+    }
+
+    if (product) {
+      setCachedProduct(product);
+    } else {
+      cacheTimeout.current = window.setTimeout(() => {
+        cacheTimeout.current = undefined;
+        setCachedProduct(product);
+      }, ANIM_DURATON_MS);
     }
   }, [product]);
 
@@ -24,9 +42,9 @@ const ProductModal = ({ product, onSubmit, ...props }: {
     isOpen={!!product}
     {...props}
   >
-    {product
+    {cachedProduct
     && <>
-      <div className='text-xl font-bold'>{product.title}</div>
+      <div className='text-xl font-bold'>{cachedProduct.title}</div>
 
       <div className='flex items-center justify-center space-x-5 mt-8'>
         <IconButton
