358dee961a5a556dd6f5a257fd58472eaa4999b7
Alen Ajam <alen.ajam@live.com>
2023-04-01 16:00:45 +0200

    checkbox: removed internal checked state, toggle check icon with css instead of js

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,33 +1,27 @@
 import * as React from 'react';
-import { ChangeEventHandler, useState } from 'react';
 import classNames from 'classnames';
 import { CheckIcon } from '@heroicons/react/solid';
 import '../styles/components/_checkbox.scss';
 
 const Checkbox = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
-  const [checked, setChecked] = useState(props.checked);
-
   const commonClasses = 'absolute top-0 bottom-0 left-0 right-0 w-full h-full';
 
-  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setChecked(e.target.checked);
-    props.onChange?.(e);
-  };
+  const inputClasses = classNames(
+    'appearance-none bg-gray-200 rounded-md shadow-inner cursor-pointer',
+    commonClasses,
+    props.className
+  );
+
+  const iconClasses = classNames('text-primary-500 p-0.5 check-icon hidden pointer-events-none', commonClasses);
 
   return <div className='relative w-6 h-6'>
     <input
       {...props}
-      className={classNames(
-        'appearance-none bg-gray-200 rounded-md shadow-inner cursor-pointer',
-        commonClasses,
-        props.className
-      )}
+      className={inputClasses}
       type='checkbox'
-      onChange={onChange}
     />
 
-    {checked
-    && <CheckIcon className={classNames('text-primary-500 p-0.5 check-icon', commonClasses)} />}
+    <CheckIcon className={iconClasses} />
   </div>;
 };
 
